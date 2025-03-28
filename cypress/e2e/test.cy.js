81a9c5afdafd100cd01b2b2a8eb0f7eb497d1554
Eduardo Lacerda <linlaslacerda@gmail.com>
2025-03-27 20:56:18 -0400

    Adicionado 3 testes

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,8 +1,25 @@
-describe('',()=>{
-    beforeEach(()=>{
+describe('Teste de Tarefas', () => {
+    beforeEach(() => {
         cy.visit('http://127.0.0.1:5500/index.html')
     })
-    it('Adicionar uma tarefa', ()=>{
-        
+    it('Deve adicionar uma tarefa a lista', () => {
+        cy.get('#todo_title').type('Fazer compras');
+        cy.contains('Criar tarefa').click();
+        cy.get('[x-text="todo.task"]').contains('Fazer compras').should('exist');
+
     })
+    it('Adicionar mais tarefas', () => {
+        const tarefas = ['Fazer exercicios', 'Comer', 'Tomar banho'];
+        tarefas.forEach((tarefa) => {
+            cy.get('#todo_title').type(tarefa);
+            cy.contains('Criar tarefa').click();
+            cy.get('[x-text="todo.task"]').contains(tarefa).should('exist');
+        });
+    });
+
+    it('Deve garantir que a lista está vazia ao carregar a página', () => {
+        cy.get('table tbody tr').should('not.exist');
+    });
+
 });
+
