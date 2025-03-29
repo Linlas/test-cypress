6be8a6662cbba59af7f1ad05f2bd06832501bca4
Luis Eugênio <123472957+louiseugenes@users.noreply.github.com>
2025-03-29 07:44:57 -0300

    feat: new tests.

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -21,4 +21,27 @@ describe('Teste de Tarefas', () => {
         cy.get('table tbody tr').should('not.exist');
     });
 
+    it('Deve permitir excluir uma tarefa', () => {
+        cy.get('#todo_title').type('Tarefa para excluir');
+        cy.get('.bg-white > .col-auto > .btn').click();
+        cy.get('.form-check-input').click();
+        cy.get('.text-end > .btn').click();
+        cy.get('[x-text="todo.task"]').contains('Tarefa para excluir').should('not.exist');
+    });
+
+    it('Deve marcar uma tarefa como concluída', () => {
+        cy.get('#todo_title').type('Tarefa para concluir');
+        cy.get('.bg-white > .col-auto > .btn').click();
+    });
+
+    it('Não deve adicionar tarefa com título vazio', () => {
+        cy.get('.bg-white > .col-auto > .btn').click();
+        cy.get('table tbody tr').should('not.exist');
+    });
+
+    it('Deve limpar o campo de input após adicionar uma tarefa', () => {
+        cy.get('#todo_title').type('Nova tarefa');
+        cy.get('.bg-white > .col-auto > .btn').click();
+        cy.get('#todo_title').should('have.value', '');
+    });
 });
